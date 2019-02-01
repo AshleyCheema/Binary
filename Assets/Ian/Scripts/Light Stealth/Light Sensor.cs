cc1751e26791a4154aa9fab24a390ed9339f64e4
IHudson <cuffy94@gmail.com>
2019-02-01 12:54:51 +0000

    Started light stealth

---
+++ Assets/Ian/Scripts/Light Stealth/Light Sensor.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * 
+ * Store the percentage of light object is in
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSensor : MonoBehaviour
+{
+    [SerializeField]
+    private float lightMeter;
+
+    private SceneLight sceneLight;
+    private bool updateLightMeter = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateLightMeter();
+    }
+
+    private void UpdateLightMeter()
+    {
+        if(updateLightMeter)
+        {
+            Debug.Log("Updating light meter");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "LightSource")
+        {
+            sceneLight = other.gameObject.transform.parent.gameObject.GetComponent<SceneLight>();
+            updateLightMeter = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "LightSource")
+        {
+            sceneLight = null;
+        }
+    }
+}
---
+++ Assets/Ian/Scripts/Light Stealth/Scene Light.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * 
+ * This script should be attached to all lights that need intacting with 
+ * the light meter
+ * 
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class SceneLight : MonoBehaviour
+{ 
+
+}
