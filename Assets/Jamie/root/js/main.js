7a950c3ab4c51d5f4d5b0baf7bc6977927a778b0
Jamie <itsdeadjim@hotmail.co.uk>
2019-02-05 11:16:04 +0000

    Initial web commit
    First push for the front end website.
    Using Bootstrap, ThreeJS and Owl Carousel.

---
+++ Assets/Jamie/root/js/main.js
@@ -0,0 +1,74 @@
+$(window).on('load', function () {;
+    console.log('refresh');
+    setTimeout(scroll, 0);
+
+});
+
+$('.slide-show').owlCarousel({
+    autoplay: true,
+    items: 1,
+    loop: true,
+    navigation: true,
+    slideSpeed: 300,
+    paginationSpeed: 400,
+    singleItem: true,
+    autoHeight: true
+});
+
+$('.meet-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    margin: 10,
+    singleItem: false,
+    slideSpeed: 500,
+    paginationSpeed: 200,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 4
+        }
+    }
+})
+
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+        invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+        el: '.blog-slider__pagination',
+        clickable: true,
+    }
+});
+
+$('#start').click(function () {
+    $('html, body').animate({
+        scrollTop: $("#navbar").offset().top
+    }, 300);
+    setTimeout(deleteSpace, 300)
+
+
+});
+
+function deleteSpace() {
+    console.log('deleted');
+    $('#space').hide();
+    $('#start').hide();
+    $('html, body').animate({
+        scrollTop: $("#navbar").offset().top
+    }, 0);
+}
+
+function scroll() {
+    $('html, body').animate({
+        scrollTop: $("#space").offset().top
+    }, 0);
+}
