7f371778dc53e393f1becab1ac3be965a4b0f422
Jamie <itsdeadjim@hotmail.co.uk>
2019-02-07 20:10:21 +0000

    Website: Frontend
    Overhaul of Frontend
    3.5 hours.

--- Website/Jamie/root/js/main.js
+++ Website/Jamie/root/js/main.js
@@ -24,14 +24,17 @@ $('.meet-carousel').owlCarousel({
     slideSpeed: 500,
     paginationSpeed: 200,
     responsive: {
-        0: {
+        425: {
             items: 1
         },
-        600: {
-            items: 3
+        768: {
+            items: 2
         },
-        1000: {
+        1024: {
             items: 4
+        },
+        1440: {
+            items: 6
         }
     }
 })
@@ -50,11 +53,11 @@ var swiper = new Swiper('.blog-slider', {
     }
 });
 
-/* $('#start').click(function () {
+$('#start').click(function () {
     $('html, body').animate({
         scrollTop: $("#navbar").offset().top
     }, 300);
-    setTimeout(deleteSpace, 300)
+    /*setTimeout(deleteSpace, 300)*/
 
 
 });
@@ -62,14 +65,30 @@ var swiper = new Swiper('.blog-slider', {
 function deleteSpace() {
     console.log('deleted');
     $('#space').hide();
-    $('#start').hide();
+
     $('html, body').animate({
         scrollTop: $("#navbar").offset().top
     }, 0);
 }
 
-function scroll() {
+/* function scroll() {
     $('html, body').animate({
         scrollTop: $("#space").offset().top
     }, 0);
 } */
+
+var $document = $(document),
+    $element = $('#start'),
+    className = 'hasScrolled';
+
+$(document).scroll(function () {
+    if ($(document).scrollTop() >= 200) {
+        // user scrolled 50 pixels or more;
+        // do stuff
+        $('#start').fadeOut();
+        $('#title-text').fadeOut();
+    } else {
+        $('#start').fadeIn();
+        $('#title-text').fadeIn();
+    }
+});
