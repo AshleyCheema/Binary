5f4de1dc43f006b8ada0088bd5db4d72c6546824
James Barrett <47109031+JBarrett93@users.noreply.github.com>
2019-02-12 15:16:27 +0000

    temp fog of war system now hides enemies behind walls

--- Assets/James/PlayerFOV.cs
+++ Assets/James/PlayerFOV.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerFOV : MonoBehaviour
 {
 	
+	public LayerMask layerMask;
 	private Camera playerCam;
 	private GameObject[] ignoreObjs;
 	
     void Start()
     {
+		//initialise variables
 		playerCam = gameObject.GetComponentInChildren<Camera>();
         ignoreObjs = GameObject.FindGameObjectsWithTag("Player");
     }
@@ -18,12 +20,22 @@ public class PlayerFOV : MonoBehaviour
     {
         Plane[] planes = GeometryUtility.CalculateFrustumPlanes(playerCam);
 		
+		//loop through each object with the player tag
 		foreach(GameObject go in ignoreObjs){
-			if(go != gameObject){
+			if(go != gameObject){ //ignore if the object is the client
 				Collider goCollider = go.GetComponent<Collider>();
 				
-				if(GeometryUtility.TestPlanesAABB(planes, goCollider.bounds)){
-					go.GetComponent<Renderer>().enabled = true;
+				//test if the collider is clipping the camera
+				if(GeometryUtility.TestPlanesAABB(planes, goCollider.bounds)){ 
+					
+					//cast a ray that checks if a wall is in the way
+					RaycastHit hit;
+					if (Physics.Linecast(transform.position, go.transform.position, out hit, layerMask) && hit.transform.gameObject == go)
+					{
+						go.GetComponent<Renderer>().enabled = true;
+					}else{
+						go.GetComponent<Renderer>().enabled = false;
+					}
 				}else{
 					go.GetComponent<Renderer>().enabled = false;
 				}
