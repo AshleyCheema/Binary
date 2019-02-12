f8d58a5ed548b24330b036da476494159cfd7d02
James Barrett <47109031+JBarrett93@users.noreply.github.com>
2019-02-12 14:32:24 +0000

    Added temp FOV system
    Hides objects not in view of camera, use this until field of view is fully implemented

---
+++ Assets/James/PlayerFOV.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFOV : MonoBehaviour
+{
+	
+	private Camera playerCam;
+	private GameObject[] ignoreObjs;
+	
+    void Start()
+    {
+		playerCam = gameObject.GetComponentInChildren<Camera>();
+        ignoreObjs = GameObject.FindGameObjectsWithTag("Player");
+    }
+
+    void Update()
+    {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(playerCam);
+		
+		foreach(GameObject go in ignoreObjs){
+			if(go != gameObject){
+				Collider goCollider = go.GetComponent<Collider>();
+				
+				if(GeometryUtility.TestPlanesAABB(planes, goCollider.bounds)){
+					go.GetComponent<Renderer>().enabled = true;
+				}else{
+					go.GetComponent<Renderer>().enabled = false;
+				}
+			}
+		}
+    }
+}
