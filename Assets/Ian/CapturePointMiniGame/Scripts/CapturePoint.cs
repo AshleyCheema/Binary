165bf7f770fcbe6b506662a7bf87f329cf2d1248
I Hudson <cuffy94@gmail.com>
2019-02-19 18:27:07 +0000

    Added basic control points

---
+++ Assets/Ian/CapturePointMiniGame/Scripts/CapturePoint.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Author: Ian Hudson
+ * Description: 
+ * Created: 19/02/2019
+ * Edited By: Ian
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapturePoint : MonoBehaviour
+{
+    [SerializeField]
+    private bool spyIsCapturing = false;
+
+    [SerializeField]
+    private float capturePercentage = 0.0f;
+
+    [SerializeField]
+    private float captureMulitiplier = 3.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(spyIsCapturing)
+        {
+            capturePercentage += (1.0f * captureMulitiplier) * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Spy")
+        {
+            spyIsCapturing = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Spy")
+        {
+            spyIsCapturing = false;
+            capturePercentage = Mathf.RoundToInt(capturePercentage);
+        }
+    }
+}
