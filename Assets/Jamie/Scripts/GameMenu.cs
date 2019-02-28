976ff547f1edc16dc0b0f748adb503bbe0d57ec4
Jamie <itsdeadjim@hotmail.co.uk>
2019-02-28 16:57:22 +0000

    In-game Menu created
    Created an in game menu in a scene called Game Menu. Inside is an object called "GameMenu", this could be made into a prefab.

---
+++ Assets/Jamie/Scripts/GameMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+    public void ResumeGame()
+    {
+        //method to hide the menu scene.
+    }
+
+    public void QuitGame()
+    {
+        //Load the main menu when called
+        Scene MainMenu = SceneManager.GetSceneByName("MainMenu");
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+}
--- Assets/Jamie/Scripts/MainMenu.cs
+++ Assets/Jamie/Scripts/MainMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class HomeMainMenu : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
