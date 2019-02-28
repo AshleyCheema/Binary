a2124f6a6043e7b80c1254144244b51c8f15edd6
Jamie <itsdeadjim@hotmail.co.uk>
2019-02-28 15:42:24 +0000

    Main menu update
    Added functionality so that the start button plays the next scene in the build index.
    Settings and quit buttons also functional

---
+++ Assets/Jamie/Scripts/HomeMainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeMainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //This loads the next scene in the build index order
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
