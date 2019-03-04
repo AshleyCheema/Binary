891d97d0aba7cedd8a50e4a10f34408283b2e796
Jamie <itsdeadjim@hotmail.co.uk>
2019-03-04 21:36:09 +0000

    Test data update
    Created a working example of a login system using Unity, PHP and MySQL.
    
    *This currently only works with local server db
    currently using MAMP
    
    location of php files:
    C:/MAMP/htdocs/sqlconnect/
    
    download:
    https://www.mamp.info/en/

---
+++ Assets/Jamie/Scenes/Test Data/Scripts/SqlMenuScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SqlMenuScript : MonoBehaviour
+{
+    public void GoToRegister()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Jamie/Scenes/Test Data/Scripts/SqlRegistration.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SqlRegistration : MonoBehaviour
+{
+    public InputField nameField;
+    public InputField passwordField;
+
+    public Button submitButton;
+
+    public void CallRegister()
+    {
+        StartCoroutine(Register());
+    }
+
+    private IEnumerator Register()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("name", nameField.text);
+        form.AddField("password", passwordField.text);
+
+        WWW www = new WWW("http://localhost/sqlconnect/register.php", form);
+        yield return www;
+
+        if (www.text == "0")
+        {
+            Debug.Log("User Created Successfully.");
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+        else
+        {
+            Debug.Log("User Creation failed. Error #" + www.text);
+        }
+    }
+
+    public void VerifyInputs()
+    {
+        submitButton.interactable = (nameField.text.Length >= 8 && passwordField.text.Length >= 8);
+    }
+}
