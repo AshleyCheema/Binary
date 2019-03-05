72e37c29cc266815c2f4fc3beb606ebe3c32b4b6
Jamie <itsdeadjim@hotmail.co.uk>
2019-03-05 10:13:30 +0000

    DBManager
    Added functionality to test scene allowing users to create a MySQL account and log in via account.

---
+++ Assets/Jamie/Scenes/Test Data/Scripts/SqlLogin.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SqlLogin : MonoBehaviour
+{
+    public InputField nameField;
+    public InputField passwordField;
+
+    public Button submitButton;
+
+    public void CallLogin()
+    {
+        StartCoroutine(LoginPlayer());
+    }
+
+    private IEnumerator LoginPlayer()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("name", nameField.text);
+        form.AddField("password", passwordField.text);
+
+        WWW www = new WWW("http://localhost/sqlconnect/login.php", form);
+        yield return www;
+
+        if (www.text[0] == '0')
+        {
+            //Debug.Log("Successfully Logged In");
+            DBManager.username = nameField.text;
+            DBManager.score = int.Parse(www.text.Split('\t')[1]);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+        else
+        {
+            Debug.Log("User login failed. Error #" + www.text);
+        }
+    }
+
+    public void VerifyInputs()
+    {
+        submitButton.interactable = (nameField.text.Length >= 8 && passwordField.text.Length >= 8);
+    }
+}
--- Assets/Jamie/Scenes/Test Data/Scripts/SqlMenuScript.cs
+++ Assets/Jamie/Scenes/Test Data/Scripts/SqlMenuScript.cs
@@ -2,9 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class SqlMenuScript : MonoBehaviour
 {
+    public TMP_Text playerDisplay;
+
+    private void Start()
+    {
+        if (DBManager.LoggedIn)
+        {
+            playerDisplay.text = "Player: " + DBManager.username;
+        }
+    }
+
     public void GoToRegister()
     {
         SceneManager.LoadScene(1);
