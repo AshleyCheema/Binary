c92fc926cb66207ccc8a08da5754bf85e9167652
Jamie <itsdeadjim@hotmail.co.uk>
2019-03-12 14:15:58 +0000

    UI: Added cooldown
    Added a cooldown script to the buttons that allow for customisable cooldown time so actions can only be called within a certain period.

---
+++ Assets/Jamie/Scripts/CooldownScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class CooldownScript : MonoBehaviour
+{
+    public Image CooldownImage;
+    public float CooldownTime;
+    public Button Action;
+
+    private float currentCooldown = 0;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Action.interactable = false;
+        currentCooldown = 0;
+        CooldownImage.fillAmount = Mathf.InverseLerp(0, 1, CooldownTime);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (currentCooldown < CooldownTime)
+        {
+            currentCooldown += Time.deltaTime;
+            CooldownImage.fillAmount = currentCooldown / CooldownTime;
+        }
+        if (CooldownImage.fillAmount == 1 && Action.interactable == false)
+        {
+            Action.interactable = true;
+            Debug.Log("Cooldown finished");
+            CooldownImage.color = new Color32(103, 201, 255, 255);
+            Invoke("ResetColour", 0.5F);
+        }
+    }
+
+    private void ResetColour()
+    {
+        CooldownImage.color = new Color32(255, 255, 255, 255);
+    }
+
+    public void ActionClicked()
+    {
+        Debug.Log("Skill '" + Action.name + "' has been clicked");
+        Action.interactable = false;
+        currentCooldown = 0;
+    }
+}
