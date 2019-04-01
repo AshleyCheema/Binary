75bf6451476190f08ff8e0df46591b5796a686ac
Ashley <ashleyycheemaa@hotmail.com>
2019-04-01 15:06:53 +0100

    Altered door colliers and speed

--- Assets/DoorTrigger.cs
+++ Assets/DoorTrigger.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class DoorTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Spy" || other.gameObject.tag == "Merc")
