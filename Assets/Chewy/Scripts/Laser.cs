e3e6804f0d89200aeb5ab0c90c04c42d05dccec0
Ashley <ashleyycheemaa@hotmail.com>
2019-04-03 17:05:13 +0100

    Laser pointer works. Does not change colour

---
+++ Assets/Chewy/Scripts/Laser.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if(hit.collider)
+            {
+                lineRenderer.SetPosition(1, hit.point);
+            }
+        }
+        else
+        {
+            lineRenderer.SetPosition(1, transform.forward * 500);
+        }
+    }
+}
