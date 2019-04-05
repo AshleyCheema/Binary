24970c04dd53a86f25d3035a9e46b1dc3645475a
Chewy <ashleyycheemaa@hotmail.com>
2019-04-05 19:17:47 +0100

    Laser pointer detects Spy

--- Assets/Chewy/Scripts/Laser.cs
+++ Assets/Chewy/Scripts/Laser.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
     private LineRenderer lineRenderer;
+    Gradient gradientRed = new Gradient();
+    Gradient gradientGreen = new Gradient();
+
     // Start is called before the first frame update
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
+
+        gradientRed.SetKeys(new GradientColorKey[] { new GradientColorKey(Color.red, 0.0f), new GradientColorKey(Color.red, 1.0f) }, 
+                            new GradientAlphaKey[] { new GradientAlphaKey(0.09f, 0.0f), new GradientAlphaKey(0f, 1.0f) });
+
+        gradientGreen.SetKeys(new GradientColorKey[] { new GradientColorKey(Color.green, 0.0f), new GradientColorKey(Color.green, 1.0f) },
+                              new GradientAlphaKey[] { new GradientAlphaKey(0.09f, 0.0f), new GradientAlphaKey(0.0f, 1f) });
     }
 
     // Update is called once per frame
@@ -21,6 +30,12 @@ public class Laser : MonoBehaviour
             if(hit.collider)
             {
                 lineRenderer.SetPosition(1, hit.point);
+                lineRenderer.colorGradient = gradientGreen;
+
+            }
+            if (hit.collider.tag == "Spy")
+            {
+                lineRenderer.colorGradient = gradientRed;
             }
         }
         else
