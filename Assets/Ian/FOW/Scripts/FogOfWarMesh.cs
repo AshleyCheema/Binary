2c205603019cc89569996957548194e7e6be5e81
Ian Hudson <cuffy94@gmail.com>
2019-07-29 21:47:10 +0100

    Update FogOfWarMesh

--- Assets/Ian/FOW/Scripts/FogOfWarMesh.cs
+++ Assets/Ian/FOW/Scripts/FogOfWarMesh.cs
@@ -8,7 +8,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FogOgWarMesh : MonoBehaviour
+public class FogOfWarMesh : MonoBehaviour
 {
     public float viewRadius;
     [Range(0, 360)]
